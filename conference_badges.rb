0acf67a0bd42aac99b48ef534e0a9468ba466b40
kenneth gil <astronori@gmail.com>
2019-11-21 03:27:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-conference_badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+conference_badges = {"Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"}
 assign_rooms = 1
 conference_badges.each do |assign_rooms|
   puts "Hello, #{conference_badges}! You'll be assigned to room #{assign_rooms}!"
