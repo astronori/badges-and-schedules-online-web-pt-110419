3870ce1b9c0b225a4a1da4c16429cf084c87f0fc
kenneth gil <astronori@gmail.com>
2019-11-21 02:47:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,6 @@
 conference_badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 assign_rooms = 1
 conference_badges.each do |assign_rooms|
-  puts "Hello, #{conference_badges}! You'll be assigned to room #{assign_rooms}"
+  puts "Hello, #{conference_badges}! You'll be assigned to room #{assign_rooms}!"
   counter = 1
 end
-
-brothers = ["Tim", "Tom", "Jim"]
-brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
