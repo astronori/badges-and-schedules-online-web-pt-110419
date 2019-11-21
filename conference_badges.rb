3266a58bf856784fc3bbe7fd03b1070987fa904d
kenneth gil <astronori@gmail.com>
2019-11-21 02:39:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-speakers = ["Edsger", "Ada", "Charles", ""Alan", Grace, Linus, and Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", Matz]
 counter = 1
 brothers.each do |brother|
   puts "Hello,  #{}"
