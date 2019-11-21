bdfedff76af465929103cfe279845fc31b22fccb
kenneth gil <astronori@gmail.com>
2019-11-21 02:42:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 assign_rooms = 1
 speakers.each do |assign_rooms|
   puts "Hello, #{speakers}! "
-  puts "Stop hitting yourself #{brother}!"
+  puts "Stop hitting yourself #{speakers}!"
   counter += 1
 end
