67160bc8fe07acd407b78b11d29cbb61992523ae
kenneth gil <astronori@gmail.com>
2019-11-21 02:46:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,6 @@ conference_badges.each do |assign_rooms|
   puts "Hello, #{conference_badges}! You'll be assigned to room #{assign_rooms}"
   counter = 1
 end
+
+brothers = ["Tim", "Tom", "Jim"]
+brothers.each{|brother| puts "Stop hitting yourself #{brother}!"}
